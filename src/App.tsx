4e3834dcd6f97841be83dd8c8e6f15972284fdab
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-01-12 21:33:11 +0530

    types and default data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+type Priority = 'p1' | 'p2' | 'p3';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
 function App() {
-  return <div>Hello World!</div>;
+  const tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Learn React',
+      isCompleted: true,
+      priority: 'p1',
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
