2f8c6daccd8128909bcd570c6db37d9ef9b5c02b
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-02-01 00:39:40 +0530

    timer

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 type TaskListProps = {
   header?: React.ReactNode;
 };
@@ -6,9 +8,20 @@ export default function TaskList({
   header,
   children,
 }: React.PropsWithChildren<TaskListProps>) {
+  const [secondsPassed, setSecondsPassed] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsPassed((secondsPassed) => secondsPassed + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       {header}
+      <p>Seconds passed: {secondsPassed}</p>
       <ul>{children}</ul>
     </>
   );
